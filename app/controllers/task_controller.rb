a19a84622d84f0690b7e1df94b8e134382abe837
EC2 Default User <ec2-user@ip-172-31-31-65.ap-northeast-1.compute.internal>
2022-04-12 07:49:59 +0000

    C4D1完了

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,2 +1,16 @@
 class TaskController < ApplicationController
+  before_action :set_user
+  before_action :set_task, only: %i(show edit update destroy)
+  before_action :logged_in_user
+  before_action :correct_user
+  
+  def index
+    @tasks = @user.tasks
+  end
+  
+  private
+  
+  def set_user
+      @user = User.find(params[:user_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+
   root 'static_pages#top'
   get '/signup', to: 'users#new'
   
@@ -6,6 +8,7 @@ Rails.application.routes.draw do
   post   '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   
-  resources :users
+  resources :users do
   resources :tasks
+end
 end
