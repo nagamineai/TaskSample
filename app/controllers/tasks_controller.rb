5e877bdc4b0566d960f335eaf507fb0735b93e0d
EC2 Default User <ec2-user@ip-172-31-31-65.ap-northeast-1.compute.internal>
2022-05-04 23:01:35 +0000

    C7完了

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,24 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %i(show edit update destroy)
+  
   def index
     @user = User.find(params[:user_id])
     @tasks = @user.tasks
   end
   
   def show
+    @user = User.find(params[:user_id])
+  end
+  def edit
+    @user = User.find(params[:user_id])
+  end
+  def update
+    if @task.update_attributes(task_params)
+      flash[:success] = "タスクを更新しました。"
+      redirect_to user_task_url(@user, @task)
+    else
+      render :edit
+    end
   end
 
   def new
@@ -31,6 +45,13 @@ class TasksController < ApplicationController
   end
   def set_user
       @user = User.find(params[:user_id])
+  end
+  def set_task
+    @user = User.find(params[:id])
+      unless @task = @user.tasks.find_by(id: params[:id])
+        flash[:danger] = "権限がありません。"
+        redirect_to user_tasks_url @user
+      end
   end
   
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i(show edit update destroy)
-  before_action :logged_in_user, only: %i(index show edit update destroy)
+  # before_action :logged_in_user, only: %i(index show edit update destroy)
   before_action :admin_user, only: %i(index destroy)
+  # before_action :correct_user, only: %i(edit update)
+  # before_action :admin_or_correct, only: %i(show)
   
   def show
-    @user = User.find(params[:id])
   end
   
   def edit
@@ -31,4 +32,8 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
+    def set_user
+      @user = User.find(params[:id])
+    end
+  
 end
