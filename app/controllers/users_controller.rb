09240f064398765450ef4bbb5d74daf363fa4535
EC2 Default User <ec2-user@ip-172-31-31-65.ap-northeast-1.compute.internal>
2022-05-10 14:18:26 +0000

    D4勤怠8.4.1bundle installして、サンプルユーザー表示エラーとなり一旦終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,18 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i(show edit update destroy)
-  # before_action :logged_in_user, only: %i(index show edit update destroy)
+  before_action :logged_in_user, only: %i(index show edit update destroy)
   before_action :admin_user, only: %i(index destroy)
-  # before_action :correct_user, only: %i(edit update)
+  before_action :correct_user, only: %i(edit update)
   # before_action :admin_or_correct, only: %i(show)
+  
+  def index
+    @users = User.paginate(page: params[:page], per_page: 20)
+  end
   
   def show
   end
   
   def edit
-    @user = User.find(params[:id])
   end
   
   def new
@@ -20,7 +23,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      flash[:success] = '新規作成に成功しました。'
+      flash[:success] = 'ユーザーの新規作成に成功しました。'
       redirect_to @user
     else
       render :new
@@ -35,5 +38,17 @@ class UsersController < ApplicationController
     def set_user
       @user = User.find(params[:id])
     end
-  
+    # ログイン済みのユーザーか確認します。
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "ログインしてください。"
+        redirect_to login_url
+      end
+    end
+
+    # アクセスしたユーザーが現在ログインしているユーザーか確認します。
+    def correct_user
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
